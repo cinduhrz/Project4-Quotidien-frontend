e2f3958c60b67e4db2ca2a4b276d15b0733a4d82
Cindy Wong <cindywongdev@gmail.com>
2023-02-22 21:34:15 -0500

    show route completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 const Todo = ({todo}) => {
-    return <p>
-        {todo.description}
-    </p>
+    return (<div>
+        <Link to={`/todo/${todo.id}`}>
+            <h3>{todo.description}</h3>
+        </Link>
+    </div>)
 }
 
 export default Todo
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,18 @@
+import { Link, useLoaderData } from "react-router-dom"
+
 const Show = (props) => {
-    return <h1>Show</h1>
+    const todo = useLoaderData()
+
+    return (<div>
+        <h1>{todo.description}</h1>
+        <h4>{todo.date}</h4>
+        <h4>category: {todo.category}</h4>
+        <h4>status: {todo.status}</h4>
+    
+        <Link to="/">
+            <button>back</button>
+        </Link>
+    </div>)
 }
 
 export default Show
