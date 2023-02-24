5730c70962408a5540df72c93739745f6432165a
Cindy Wong <cindywongdev@gmail.com>
2023-02-24 18:16:40 -0500

    uninstalled milligram

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import "milligram"
 import { RouterProvider } from 'react-router-dom';
 import router from './router'
 
